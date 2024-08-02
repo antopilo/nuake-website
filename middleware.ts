0fd499b5f945930580ba00eb4dbf21868c52cd28
Antoine Pilote <antoinep@momentfactory.com>
2024-08-02 10:28:47 -0400

    Forgot to push middleware  yesterday

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  // Set hostname from environment variable
+  req.nextUrl.hostname = new URL(process.env.NEXTAUTH_URL!).hostname;
+
+  console.log( req.nextUrl.hostname)
+  console.log("MIDDLEWARE");
+  // Return the response
+  return NextResponse.next();
+}
+
+// Optionally define your matcher to apply this middleware to specific paths
+export const config = {
+  matcher: ['/api/:path*', '/protected/:path*'], // Update paths as needed
+};
